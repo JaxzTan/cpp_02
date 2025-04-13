991b15841596a042634a2b94a37ce3cd23ae082e
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-04-13 14:35:30 +0800

    feat: added comment in ex00/fixed.cpp

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:03:20 by chtan             #+#    #+#             */
-/*   Updated: 2025/04/02 15:35:19 by chtan            ###   ########.fr       */
+/*   Updated: 2025/04/13 14:15:56 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ Fixed::Fixed(const Fixed &toCopy) {
 }
 
 // copy assignment operator
+// Checks if this != &rhs (not self-assigning)
 Fixed &Fixed::operator=(const Fixed &rhs) {
 	std::cout << GREEN_H << "Copy assignment operator called" << RESET_H <<std::endl;
 	if (this == &rhs)
