a40f99a5b0645c913eb61c23e93c6e43e43e6152
Jaxz Tan <jaxztan@Jaxzs-MacBook-Pro.local>
2025-04-18 20:23:59 +0800

    fix: change ex00 copy constructor without using copy assigement

--- ex00/src/Fixed.cpp
+++ ex00/src/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: chtan <chtan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:03:20 by chtan             #+#    #+#             */
-/*   Updated: 2025/04/13 14:15:56 by chtan            ###   ########.fr       */
+/*   Updated: 2025/04/17 11:11:54 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,17 @@ Fixed::Fixed(void) {
 	return ;
 }
 
-// copy constructor
-Fixed::Fixed(const Fixed &toCopy) {
+Fixed::Fixed(const Fixed &tocopy) : _rawBits(tocopy._rawBits) {
 	std::cout << GREEN_H << "Copy constructor called" << RESET_H <<std::endl;
-	*this = toCopy;
 	return ;
 }
+
+// copy constructor
+// Fixed::Fixed(const Fixed &toCopy) {
+// 	std::cout << GREEN_H << "Copy constructor called" << RESET_H <<std::endl;
+// 	*this = toCopy;
+// 	return ;
+// }
 
 // copy assignment operator
 // Checks if this != &rhs (not self-assigning)
